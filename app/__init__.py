59262ca40d6e398732aade53c59b66efff07d24a
Mark Keele <mark@keele.codes>
2019-07-23 17:47:41 -0600

    refactor domain logic to SenPy class

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from app.sen_py import SenPy
 
 def make_app():
 	app = Flask(__name__)
-	loaded_models = {}
+	senpy = SenPy()
 
 	@app.route('/score', methods=["POST"])
 	def score():
@@ -16,25 +16,9 @@ def make_app():
 		elif 'model' not in data or 'tweets' not in data:
 			return jsonify(error="Forbidden", reason="Invalid request format"), 403
 		else:
-			model = load_model(loaded_models, data['model'])
-			tweets = predict_tweets(model, data['tweets'])
+			model = senpy.get_model(data['model'])
+			tweets = senpy.predict_tweets(model, data['tweets'])
 			return jsonify(model=model.__module__, tweets=tweets), 200
-
-	def load_model(loaded_models, requested_model):
-		if requested_model in loaded_models:
-			return loaded_models[requested_model]
-		else:
-			model = SenPy.load_model(requested_model)
-			model = model.load()
-			loaded_models[requested_model] = model
-			return model
-
-	def predict_tweets(model, tweets):
-		for tweet in tweets:
-			score = model.predict(tweet['text'])
-			tweet['score'] = score
-			tweet['sentiment'] = 'positive' if score > 0 else 'negative'
-		return tweets
 
 	return app
 
