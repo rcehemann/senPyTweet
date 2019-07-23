07456c0197535b61c09a65b3035d5612305887f4
Mark Keele <mark@keele.codes>
2019-07-23 17:06:53 -0600

    refactor prediction requests to functions

--- app/__init__.py
+++ app/__init__.py
@@ -1,50 +1,40 @@
 # senPyTweet web-layer API
-#
-# rcehemann
-##########################
 
 from flask import Flask, request, jsonify
 from app.sen_py import SenPy
 
 def make_app():
-	"""
-	make a Flask object with defined endpoints and model
-	"""
 	app = Flask(__name__)
 	loaded_models = {}
 
 	@app.route('/score', methods=["POST"])
 	def score():
-		data = request.get_json()
+		data = request.get_json(silent=True, cache=False)
 
 		if data is None:
-			return jsonify(
-				error='Content-Type must be application/json'
-			), 403
-
-		if 'tweets' not in data:
-			return jsonify(
-				error='Request data must contain "tweets"'
-			), 400
-
-		if data['model'] in loaded_models:
-			model = loaded_models[data['model']]
+			return jsonify(error="Forbidden", reason="Accepts application/json"), 403
+		elif 'model' not in data or 'tweets' not in data:
+			return jsonify(error="Forbidden", reason="Invalid request format"), 403
 		else:
-			model = SenPy.load_model(data['model'])
+			model = load_model(loaded_models, data['model'])
+			tweets = predict_tweets(model, data['tweets'])
+			return jsonify(model=model.__module__, tweets=tweets), 200
+
+	def load_model(loaded_models, requested_model):
+		if requested_model in loaded_models:
+			return loaded_models[requested_model]
+		else:
+			model = SenPy.load_model(requested_model)
 			model = model.load()
-			loaded_models[data['model']] = model
+			loaded_models[requested_model] = model
+			return model
 
-		for tweet in data['tweets']:
-			if len(tweet['text']) > 0:
-				score = model.predict(tweet['text'])
-				tweet.update(
-					dict(
-						score = score,
-						sentiment = 'positive' if score > 0 else 'negative'
-					)
-				)
-
-		return jsonify(data), 200
+	def predict_tweets(model, tweets):
+		for tweet in tweets:
+			score = model.predict(tweet['text'])
+			tweet['score'] = score
+			tweet['sentiment'] = 'positive' if score > 0 else 'negative'
+		return tweets
 
 	return app
 
